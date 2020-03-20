ffa01da9ddadbc18ac0be8a55ff5dd67fb3a1914
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2020-03-20 17:15:41 +0900

    Use go-cloud for KVS

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/tosik/go-react-graphql-sandbox/server
 
 go 1.14
+
+require (
+	gocloud.dev v0.19.0
+	gocloud.dev/docstore/mongodocstore v0.19.0
+)
--- server/main.go
+++ server/main.go
@@ -2,8 +2,43 @@ package main
 
 import (
   "log"
+  "context"
+
+  "gocloud.dev/docstore"
+  _ "gocloud.dev/docstore/mongodocstore"
 )
 
+type Book struct {
+  ID int
+  Title string
+  Price int
+  DocstoreRevision interface{}
+}
+
 func main() {
-  log.Println("Hello, World!")
+  ctx := context.Background()
+
+  {
+    url := "mongo://foo/books?id_field=ID"
+
+    coll, err := docstore.OpenCollection(ctx, url)
+    if err != nil {
+      log.Fatalln(err)
+    }
+
+    books := []Book{
+      { ID: 1, Title: "Alice In Wonderland", Price: 123 },
+      { ID: 2, Title: "Cinderella", Price: 345 },
+    }
+
+    for _, book := range books {
+      log.Println(book)
+      err = coll.Put(ctx, &book)
+      if err != nil {
+        log.Fatalln(err)
+      }
+    }
+
+    defer coll.Close()
+  }
 }
