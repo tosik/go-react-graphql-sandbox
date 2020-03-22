e41efabe975ffeaf170069062a229bdc15abc379
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2020-03-22 20:15:00 +0900

    Use Apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const BOOKS_QUERY = gql`
+query {
+  books {
+    title
+    id
+    price
+    foo
+  }
+}
+`
+
+const CREATE_BOOK_MUTATION = gql`
+mutation createBook {
+  createBook(input : {
+    title: "竹取物語",
+    price: 1980,
+    foo: {
+      koreha: "nandemo",
+      dekimasu: 9090909
+    }
+  }) {
+    id
+    title
+    price
+    foo
+  }
+}
+`
+
+const Books: React.FC = () => {
+  var { loading, error, data } = useQuery(BOOKS_QUERY)
+
+  if (loading) return (<p>Loading...</p>)
+  if (error) return (<p>Error </p>)
+
+  var list = []
+  for (var item of data.books) {
+    list.push(<li>{item.title}: {item.price}円</li>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{list}</ul>
     </div>
+  )
+}
+
+const CreateBookButton: React.FC = () => {
+  var [createBook, { data }] = useMutation(CREATE_BOOK_MUTATION)
+
+  return (
+    <button onClick={
+      e => {
+        e.preventDefault()
+        createBook()
+      }
+    }> Create a book</button>
+  )
+}
+
+const App: React.FC = () => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:8080/query',
+    request: operation => {
+      operation.setContext({});
+    }
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app </h2>
+        <Books />
+        <CreateBookButton />
+      </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+export default App
